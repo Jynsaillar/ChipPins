6de5fbe8170f556d04a0180511781070ce1b6ced
Jynsaillar <7015218+Jynsaillar@users.noreply.github.com>
2023-04-21 01:16:16 +0200

    feat/pintype: Implement actual PinType type in ChipPin class

--- src/ChipPins.h
+++ src/ChipPins.h
@@ -1,12 +1,22 @@
 #ifndef __ChipPins_H__
 #define __ChipPins_H__
 
+#include "PinTypes.h"
+
 namespace Jynsaillar::ChipPins
 {
-    class ChipPin {
-        private:
-        public:
-    }
+    class ChipPin
+    {
+    private:
+    public:
+        const char *Name;              // Pin name, e.g. CE, SCL, SDA, CS, ...
+        bool Enabled;                  // Pin enabled/disabled?
+        PinTypes::PinType PinType;               // Analog/Digital/NC?
+        ActivationType ActivationType; // Active High/Active Low?
+        Signal Signal;                 // High/Low/HighImpedance?
+
+        operator bool() const;
+    };
 } // namespace Jynsaillar::ChipPins
 
 #endif // __ChipPins_H__
