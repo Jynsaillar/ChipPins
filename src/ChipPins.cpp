93762ac7856549061fec2a8bfed6db2d55318712
Jynsaillar <7015218+Jynsaillar@users.noreply.github.com>
2023-04-21 00:36:48 +0200

    feat/scaffolding: Set up the basic source/header structure, with namespace & include guard

---
+++ src/ChipPins.cpp
@@ -0,0 +1,4 @@
+namespace Jynsaillar::ChipPins
+{
+    
+} // namespace Jynsaillar::ChipPins
---
+++ src/ChipPins.h
@@ -0,0 +1,9 @@
+#ifndef __ChipPins_H__
+#define __ChipPins_H__
+
+namespace Jynsaillar::ChipPins
+{
+    
+} // namespace Jynsaillar::ChipPins
+
+#endif // __ChipPins_H__
