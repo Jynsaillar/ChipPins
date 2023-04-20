74d8b4d6403323ef49b57a54fcb9ac2ef616bd55
Jynsaillar <7015218+Jynsaillar@users.noreply.github.com>
2023-04-21 01:41:19 +0200

    feat(pin_signal_state_comparison): Add implementation of bool comparison operator, to allow directly checking the pin for a high/low signal via if/else

--- src/ChipPins.cpp
+++ src/ChipPins.cpp
@@ -4,6 +4,10 @@ namespace Jynsaillar::ChipPins
 
     Jynsaillar::ChipPins::ChipPin::operator bool() const
     {
+        if (!this->Enabled)
+            return false;
+
+        return (this->Signal == Signals::High); // Allows for quick comparison of a pin's logic state, without the need to access the underlying ChipPin.Signal
     }
 
 } // namespace Jynsaillar::ChipPins
